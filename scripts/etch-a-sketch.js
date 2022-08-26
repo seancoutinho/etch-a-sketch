51026c991dcf6d9c7f40896af8a2c71abefcf3ee
Sean Coutinho <seancoutinho@my.uopeople.edu>
2022-08-27 00:30:37 +0200

    reset button added to HTML

--- scripts/etch-a-sketch.js
+++ scripts/etch-a-sketch.js
@@ -1,18 +1,21 @@
-const sizeOfGrid = 106;
+const sizeOfGrid = 5;
 const container = document.querySelector('.container');
 const board = document.querySelector('.board');
 
 const createGrid = (numberofGrids) => {
     //create rows using a for-loop
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('wrapper');
+
     for (var i=0; i<numberofGrids; i++) {
         const gridRow = document.createElement('div')
         gridRow.classList.add('grid-row')
         //This for-loop will create a single square 'numberofGrids' times and add it to the gridRow
         for (var j=0; j<numberofGrids; j++) {
-            const squareDimensions = 500/sizeOfGrid
+            const squareDimensions = 500/numberofGrids
             const square = document.createElement('div')
-            square.style.width = `${squareDimensions}px`;
-            square.style.height = `${squareDimensions}px`;
+            square.style.width = `${squareDimensions}px`
+            square.style.height = `${squareDimensions}px`
             square.classList.add('square')
             square.addEventListener('mouseenter', () => {
                 square.classList.add('hovered');
@@ -20,9 +23,28 @@ const createGrid = (numberofGrids) => {
 
             gridRow.appendChild(square)
         }
-        board.appendChild(gridRow)
+        wrapper.appendChild(gridRow)
     }
+    board.appendChild(wrapper);
 }
 
-
 createGrid(sizeOfGrid);
+
+const resetButton = document.querySelector('#numberOfSquares')
+
+resetButton.addEventListener('click', () => {
+    let userSize = parseInt(prompt('Enter grid size: '));
+    while (userSize>100) {
+        userSize = parseInt(prompt('Choose a number below 100'));
+    }
+    wrapper = document.querySelector('.wrapper');
+
+    if (!wrapper) {
+        createGrid(userSize)
+    } else {
+        wrapper.remove()
+        createGrid(userSize)
+    }
+
+})
+
